0dee78b5b4962c66e0e0850cfd84c4588b9f9230
Orso Meneghini <meneghini@fusion.gat.com>
2016-05-10 10:04:12 -0700

    allows different power law normalization per ANN

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 export FANN_ROOT=../fann
 gcc -o run.exe brainfuse_run.c -O3 -L $FANN_ROOT/src -I $FANN_ROOT/src/include/ -lfloatfann -lm
 gcc -o train.exe brainfuse_train.c -O3 -L $FANN_ROOT/src -I $FANN_ROOT/src/include/ -lfloatfann -lm
-#cp brainfuse_run.c ../neuped/
+cp brainfuse_run.c ../neural/
+cp run.exe ../neural/brainfuse_run.exe
