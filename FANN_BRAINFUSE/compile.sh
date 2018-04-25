4d9a2398a1b21ef7c9059e09b35f820c35ebe17b
Orso Meneghini <meneghini@fusion.gat.com>
2018-04-25 10:20:14 -0700

    moved FANN brainfuse script to FANN_BRAINFUSE dir

--- FANN_BRAINFUSE/compile.sh
+++ FANN_BRAINFUSE/compile.sh
@@ -1,4 +1,4 @@
-export FANN_ROOT=../fann
+export FANN_ROOT=../../fann
 gcc -o brainfuse_run.exe brainfuse_run.c -O3 -L $FANN_ROOT/src -I $FANN_ROOT/src/include/ -lfloatfann -lm
 gcc -o brainfuse_train.exe brainfuse_train.c -O3 -L $FANN_ROOT/src -I $FANN_ROOT/src/include/ -lfloatfann -lm
 ln -sf brainfuse_run.exe run.exe
