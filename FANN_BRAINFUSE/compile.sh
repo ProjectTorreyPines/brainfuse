050f77d1e0efef093454553743d13948052c071b
Sterling Smith <smithsp@fusion.gat.com>
2018-08-04 00:17:03 -0700

    Link to FANN as installed (not built).  Use rpath to avoid LD_LIBRARY_PATH

--- FANN_BRAINFUSE/compile.sh
+++ FANN_BRAINFUSE/compile.sh
@@ -1,6 +1,7 @@
-export FANN_ROOT=../../fann
-gcc -o brainfuse_run.exe brainfuse_run.c -O3 -L $FANN_ROOT/src -I $FANN_ROOT/src/include/ -lfloatfann -lm
-gcc -o brainfuse_train.exe brainfuse_train.c -O3 -L $FANN_ROOT/src -I $FANN_ROOT/src/include/ -lfloatfann -lm
+#export FANN_ROOT=../../fann
+gcc -o brainfuse_run.exe brainfuse_run.c -O3 -L $FANN_ROOT/lib -I $FANN_ROOT/include/ -lfloatfann -lm -Wl,--rpath=$FANN_ROOT/lib
+gcc -o brainfuse_train.exe brainfuse_train.c -O3 -L $FANN_ROOT/lib -I $FANN_ROOT/include/ -lfloatfann -lm -Wl,--rpath=$FANN_ROOT/lib
 ln -sf brainfuse_run.exe run.exe
 ln -sf brainfuse_train.exe train.exe
-cp brainfuse_run.* ../neural/
+cp brainfuse_run.* ../../neural/
+cp brainfuse_train.* ../../neural/
